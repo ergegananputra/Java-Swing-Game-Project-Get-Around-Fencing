81d9977da48d68a494c8cd1a6b5b3a61e80c676a
1langit <langitlintangradjendra@mail.ugm.ac.id>
2023-05-31 15:05:35 +0700

    edit obstacle and soup

--- src/models/Obstacle.java
+++ src/models/Obstacle.java
@@ -1,4 +1,8 @@
 package models;
 
 public class Obstacle extends Coordinate{
+
+    public boolean isObstacle(int x, int y) {
+        return super.getCoordinateX() == x && super.getCoordinateY() == y;
+    }
 }
--- src/models/Soup.java
+++ src/models/Soup.java
@@ -1,4 +1,39 @@
 package models;
 
 public class Soup extends Coordinate{
+    private String name;
+    private int effect;
+    public static final String OUTFIT = "";
+
+    public Soup(String name, int effect) {
+        setName(name);
+        setEffect(effect);
+    }
+
+    public boolean isSoup(int x, int y) {
+        return super.getCoordinateX() == x && super.getCoordinateY() == y;
+    }
+
+    // Getter Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEffect() {
+        return effect;
+    }
+
+    public void setEffect(int effect) {
+        if (effect > 0) {
+            this.effect = effect;
+        }
+    }
+
+    public static String getOutfit() {
+        return OUTFIT;
+    }
 }
