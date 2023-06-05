5f7766aa000d2bcb00b808d5462f1b236ebca0c4
1langit <langitlintangradjendra@mail.ugm.ac.id>
2023-06-06 00:19:31 +0700

    add base home page menu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import GUI.GamePlayGUI;
+import GUI.HomePageMenuGUI;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            GamePlayGUI gamePlayGUI = new GamePlayGUI(0, true);
+            // GamePlayGUI gamePlayGUI = new GamePlayGUI(0, true);
+            HomePageMenuGUI home = new HomePageMenuGUI(0, true);
         });
     }
 }
